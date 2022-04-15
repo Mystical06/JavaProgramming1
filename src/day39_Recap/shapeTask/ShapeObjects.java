daaaa60e89b7a5fd26f970b3f4889c923136c615
Mystical033 <mustafa.aydin.uk@hotmail.com>
2022-04-16 00:55:57 +0100

    Day39- Encapsulation & Inheritance Recap

---
+++ src/day39_Recap/shapeTask/ShapeObjects.java
@@ -0,0 +1,55 @@
+package day39_Recap.shapeTask;
+
+public class ShapeObjects {
+    public static void main(String[] args) {
+
+        Square square1 = new Square(12.6);
+
+        System.out.println(square1);
+
+        System.out.println( "Area: " +  square1.area());
+        System.out.println( "Perimeter: " + square1.perimeter());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
