4e2cdde476e8e52802767d0a7dec0ba4c29d29e4
Mystical033 <mustafa.aydin.uk@hotmail.com>
2022-04-17 00:30:58 +0100

    Day39- Encapsulation & Inheritance Recap

---
+++ src/day39_Recap/cydeoTask/Person.java
@@ -0,0 +1,86 @@
+package day39_Recap.cydeoTask;
+
+public class Person {
+
+    private String name;
+    private int age;
+    private char gender;
+
+    public Person(String name, int age, char gender) {
+        setName(name);
+        setAge(age);
+        setGender(gender);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name==null){
+            System.err.println("Name cannot be null");
+            System.exit(1);
+        }
+        if (name.isEmpty() || name.isBlank()){
+            System.err.println("Invalid name");
+            System.exit(1);
+        }
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age<=0 || age>150)
+            System.err.println("Invalid age ");
+        System.exit(1);
+        this.age = age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public void eat(){
+        System.out.println(name + " is eating");
+    }
+
+    public void drink(){
+        System.out.println(name  + " is drinking");
+    }
+
+    public void sleep(){
+        System.out.println(name + " is sleeping");
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                '}';
+    }
+}
+/*
+CydeoTask:
+    1. Create a class named Person:
+            Variables:
+                name, age, gender
+
+            Encapsulate all the fields
+
+            Add a constructor to set all the fields
+
+            Methods:
+                eat()
+                drink()
+                sleep()
+                toString()
+ */
