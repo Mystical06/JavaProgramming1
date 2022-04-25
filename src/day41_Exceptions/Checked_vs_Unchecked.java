833216607303089f566224601385039785ef0f26
Mystical033 <mustafa.aydin.uk@hotmail.com>
2022-04-25 01:33:03 +0100

    Day40- Protected Access Modifier & Final Keyword

---
+++ src/day41_Exceptions/Checked_vs_Unchecked.java
@@ -0,0 +1,58 @@
+package day41_Exceptions;
+
+import day39_Recap.cydeoTask.Student;
+
+public class Checked_vs_Unchecked {
+
+    public static void main(String[] args) {
+
+     //unchecked exceptions
+
+     int a= 10;
+     int b=0;
+        // System.out.println(a/b);
+        // System.out.println("Wooden Spoon");
+
+        char[] characters = {'A', 'B', 'C'};
+        //                    0,   1,   2
+       // System.out.println(characters[99]);
+
+
+       // Student student =null;
+       // System.out.println(student.getName());
+
+        String str = "Wooden Spoon";
+        str=null;
+        //System.out.println(str.toUpperCase());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
