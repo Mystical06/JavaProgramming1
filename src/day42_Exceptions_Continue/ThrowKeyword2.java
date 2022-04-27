b0f796e6bac5e741ee1b4581b64fb15d04822789
Mystical033 <mustafa.aydin.uk@hotmail.com>
2022-04-27 02:43:46 +0100

    Day42- Exceptions Continue

---
+++ src/day42_Exceptions_Continue/ThrowKeyword2.java
@@ -0,0 +1,28 @@
+package day42_Exceptions_Continue;
+
+import java.util.NoSuchElementException;
+
+public class ThrowKeyword2 {
+    public static void main(String[] args) {
+
+
+        throw new NoSuchElementException("No such element"); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
